461576f0d6b2fe6a77d11e5bada022c062243763
Nash Maglaqui <nashmglq89@gmail.com>
2025-04-08 17:37:02 +0800

    fix: removed unused components

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import { BackgroundLines } from "@/components/ui/background-lines";
-import { CardBody, CardContainer, CardItem } from "../components/ui/3d-card";
-import { Data } from "@/lib/data/data";
+import { BackgroundLines } from "@/components/background-lines";
 
 function BackgroundLinesDemo() {
   return (
@@ -35,8 +33,10 @@ function BackgroundLinesDemo() {
       {/* Projects */}
       <div className="flex bg-black items-center justify-center w-full flex-col px-4">
         <h2 className="bg-clip-text text-transparent text-white text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
-          College Life
+          Projects
         </h2>
+
+         
       </div>
 
 
